1b8217f2dd39c81b32784ae7cb82baa94b63f92a
Caroline Macri <fishknit1@gmail.com>
2023-01-07 10:30:50 -0500

    created payment controller

---
+++ controllers/paymentsController.js
@@ -0,0 +1,8 @@
+const Payment = require('../models/paymentModel');
+const factory = require('./controllerFactory');
+
+exports.getPayment = factory.getOne(Payment);
+exports.getAllPayments = factory.getAll(Payment);
+exports.updatePayment = factory.updateOne(Payment);
+exports.deletePayment = factory.deleteOne(Payment);
+exports.createPayment = factory.createOne(Payment);
