5742ff1d90db32e6bc86e0cbdb6d9483172b04eb
Caroline Macri <fishknit1@gmail.com>
2023-02-25 18:53:00 -0500

    validation on child api routes

--- controllers/childController.js
+++ controllers/childController.js
@@ -15,21 +15,30 @@ exports.validateParent = catchAsync(async (req, res, next) => {
   const child = await Child.findById(req.params.id);
   const family = await Family.findById(child.family);
   const parent = family.parent;
-    const currentRoles = await parent.getCurrentRoles();
+  const currentRoles = await parent.getCurrentRoles();
   console.log('------------------ VALIDATING PARENT');
   console.log(req.user);
   console.log(req.params.id);
   console.log(child.family);
-    console.log(family.parent._id + typeof family.parent._id);
-    console.log(currentRoles);
+  console.log(family.parent._id + typeof family.parent._id);
+  console.log(currentRoles);
 
-  if (req.user._id.toString() == family.parent._id.toString()) {
-    console.log('you are the parent');
-  } else {
+  const isAdmin = currentRoles.includes('admin' || 'sysAdmin');
+  const isParentOfChild =
+    req.user._id.toString() == family.parent._id.toString();
+
+  if (!isAdmin) {
+    console.log('you are not admin');
+    return next(
+      new AppError('You do not have permission to perform this action', 403)
+    );
+  } else if (!isParentOfChild) {
     console.log('you are not the parent');
     return next(
       new AppError('You do not have permission to perform this action', 403)
     );
+  } else {
+      console.log('you are the parent')
   }
 
   //   const okRoles = req.user.currentRoles.filter((userRole) =>
