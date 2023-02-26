a47e8c18c1ae9cc0a8aece2d4d59a54706d83d82
Caroline Macri <fishknit1@gmail.com>
2023-02-26 14:28:45 -0500

    year model add registration and course editing logic

--- models/yearModel.js
+++ models/yearModel.js
@@ -1,20 +1,43 @@
 // npm modules
-const mongoose = require("mongoose");
-const { update } = require("./userModel");
+const mongoose = require('mongoose');
+const { update } = require('./userModel');
 
-const yearSchema = new mongoose.Schema({
-  year: {
-    type: String,
-    required: [true, "Year in the format yyyy-yyyy required"], 
+const yearSchema = new mongoose.Schema(
+  {
+    year: {
+      type: String,
+      required: [true, 'Year in the format yyyy-yyyy required'],
+    },
+    current: {
+      type: Boolean,
+      default: false,
+    },
+    registrationCloseDate: {
+      type: Date,
+      required: [true, 'registration close date required'],
+    },
+    courseEditCloseDate: {
+      type: Date,
+      required: [true, 'Course edit close date required'],
+    },
   },
-  current: {
-    type: Boolean,
-    default: false,
-  },
-  registrationCloseDate: {
-    type: Date,
-    required: [true, "registration close date required"],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+yearSchema.virtual('isRegistrationOpen').get(function () {
+  const today = Date.now();
+  const close = this.registrationCloseDate.getTime();
+
+  return (today < close);
+});
+yearSchema.virtual('isCourseEditingAllowed').get(function () {
+  const today = Date.now();
+  const close = this.courseEditCloseDate.getTime();
+
+  return (today < close);
 });
 
 yearSchema.statics.setCurrentYear = async function (year) {
@@ -29,11 +52,11 @@ yearSchema.statics.setCurrentYear = async function (year) {
 
 yearSchema.statics.findCurrentYearOnly = async function () {
   return await this.find({ current: true });
-}
+};
 yearSchema.statics.getCurrentYearValue = async function () {
   const yearRecord = await this.findOne({ current: true });
   return yearRecord.year;
-}
+};
 
-const Year = mongoose.model("Year", yearSchema);
+const Year = mongoose.model('Year', yearSchema);
 module.exports = Year;
