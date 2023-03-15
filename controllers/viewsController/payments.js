7d414e07c61e524e714bc0c33b9e02c453dcc0e6
Caroline Macri <fishknit1@gmail.com>
2023-03-15 14:47:11 -0400

    add  enrollment status to payments

--- controllers/viewsController/payments.js
+++ controllers/viewsController/payments.js
@@ -1,6 +1,4 @@
-const mongoose = require('mongoose');
-
-const catchAsync = require('../../utils/catchAsync');
+const mongoose = require('mongoose');const catchAsync = require('../../utils/catchAsync');
 const Year = require('../../models/yearModel');
 const Payment = require('../../models/paymentModel');
 const User = require('../../models/userModel');
@@ -9,6 +7,7 @@ exports.getPaymentsTable = catchAsync(async (req, res, next) => {
   const Year = require('../../models/yearModel');
   const Payment = require('../../models/paymentModel');
   const User = require('../../models/userModel');
+  const Family = require('../../models/familyModel');
 
   let { selectedYear, parentId } = req.params;
 
@@ -22,10 +21,20 @@ exports.getPaymentsTable = catchAsync(async (req, res, next) => {
   const hasParent = parentId != ' ' && typeof parentId != 'undefined';
 
   var parentPipeline = [];
+  var parent = {};
+  var family = {};
   if (hasParent) {
     parentPipeline = [
       { $match: { parent: mongoose.Types.ObjectId(parentId) } },
     ];
+
+    parent = await User.findOne({
+      _id: mongoose.Types.ObjectId(parentId),
+    });
+    family = await Family.findOne({
+      parent: parent.id,
+      year: selectedYear,
+    });
   }
 
   var payments = await Payment.aggregate()
@@ -38,6 +47,13 @@ exports.getPaymentsTable = catchAsync(async (req, res, next) => {
       as: 'parent',
     })
     .addFields({ parent: { $first: '$parent' } })
+    // .lookup({
+    //   from: 'families',
+    //   localField: 'parent.id',
+    //   foreignField: 'id',
+    //   as: 'family',
+    // })
+    // .addFields({ family: { $first: '$family' } })
     .lookup({
       from: 'users',
       localField: 'teacher',
@@ -47,12 +63,11 @@ exports.getPaymentsTable = catchAsync(async (req, res, next) => {
     .addFields({ teacher: { $first: '$teacher' } })
     .sort({ 'parent.lastName': 1, 'teacher.lastName': 1 });
 
-  const parent = await User.findOne({ _id: mongoose.Types.ObjectId(parentId) });
-
   res.status(200).render('payments/payments_table', {
     title: `Payments ${selectedYear}`,
     hasParent,
     parent,
+    family,
     payments,
     years,
     selectedYear,
@@ -60,7 +75,7 @@ exports.getPaymentsTable = catchAsync(async (req, res, next) => {
 });
 
 exports.getPaymentProfile = catchAsync(async (req, res, next) => {
-  const Payment = require('../../models/paymentModel'); 
+  const Payment = require('../../models/paymentModel');
   const User = require('../../models/userModel');
 
   let { paymentId, selectedYear, parentId } = req.params;
@@ -71,7 +86,7 @@ exports.getPaymentProfile = catchAsync(async (req, res, next) => {
     selectedYear = await Year.findOne({ current: true });
     selectedYear = selectedYear.year;
   }
-  
+
   const teachers = await User.find()
     .where(`yearRoles.${selectedYear}`)
     .equals('teacher')
@@ -111,7 +126,6 @@ exports.getPaymentProfile = catchAsync(async (req, res, next) => {
     payment = payments.pop();
   }
 
-  
   var matchParent = {};
   if (parentId) {
     matchParent = { _id: mongoose.Types.ObjectId(parentId) };
@@ -133,6 +147,6 @@ exports.getPaymentProfile = catchAsync(async (req, res, next) => {
     teachers,
     parents,
     selectedYear,
-    hasParent: (parentId !== undefined)
+    hasParent: parentId !== undefined,
   });
 });
